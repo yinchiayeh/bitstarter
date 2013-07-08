490aac997397d7f8826714392ba3f3b8af901883
Yin-Chia Yeh <yinchia.yeh@gmail.com>
2013-07-08 00:05:21 +0000

    Return index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var indexHtml = fs.readFileSync("index.html").toString();
+  response.send(indexHtml);
 });
 
 var port = process.env.PORT || 5000;
